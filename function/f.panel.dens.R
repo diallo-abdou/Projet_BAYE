27c271882cd397c1a840bc51dd78dbfae41aea73
Odin <odin.rumianowski@agroparistech.fr>
2023-12-12 14:17:52 +0100

    add initial file

---
+++ function/f.panel.dens.R
@@ -0,0 +1,9 @@
+panel.dens <- function(x, ...)
+{
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(usr[1:2], 0, 1.5) )
+    h <- density(x,adjust=1)
+    h$y<-h$y/max(h$y)
+    xlim=range(h$x)
+    lines(h, col="black", ...)
+}
